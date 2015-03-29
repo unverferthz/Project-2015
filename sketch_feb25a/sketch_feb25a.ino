bcb6f6a43c60449190fb69c96b61907bcf4e49a2
Zane Unverferth <unverferthz@gmail.com>
2015-03-30 11:46:02 +1300

    SD write error protect

--- sketch_feb25a/sketch_feb25a.ino
+++ sketch_feb25a/sketch_feb25a.ino
@@ -1,4 +1,6 @@
+#include <Time.h>
 
+#include <SD.h>
 #include "Ultrasonic.h"
 #include <FileIO.h>
 
@@ -6,18 +8,47 @@
 
 Ultrasonic ultrasonic (12,13);
 
+const int chipSelect = 53;
+const int buttonPin = 2;
+
+File dataFile;
+
 void setup() {
-   Serial.begin(9600); 
+   Serial.begin(9600);
+   pinMode(buttonPin, INPUT);
+   initSD();
+}
+
+void initSD() {
+   Serial.print("Initializing SD card...");
+   pinMode(10, OUTPUT);
+   
+   if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    return;
+  }
+  Serial.println("card initialized.");
 }
 
 void loop()
 {
- int distance = ultrasonic.Ranging(CM);
- 
- if (distance < 100)
- {
-  Serial.print(distance);
-  Serial.print("cm ");
+
+ if(!buttonState == HIGH){
+     
+   dataFile = SD.open("datalog.txt", FILE_WRITE);
+   int distance = ultrasonic.Ranging(CM);
+   
+   if(dataFile){ 
+  
+     if (distance < 100) {
+      dataFile.print("d");
+      dataFile.print(distance);
+      Serial.print(distance);
+      Serial.print("t");
+      dataFile.close();
+     }
+   }
  }
  
   delay(100);
