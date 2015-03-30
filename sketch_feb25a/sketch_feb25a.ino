717d296813aaa8bd0ce70e2f3909ed1ede0dcad8
CGHill <cameron.hill2@hotmail.com>
2015-03-31 10:37:42 +1300

    Arduino code counting
    Arduino now counts people passing sonar sensor, puts the data into a
    text file on SD card.
    Commented code.
    Cleaned up the loop code.

--- sketch_feb25a/sketch_feb25a.ino
+++ sketch_feb25a/sketch_feb25a.ino
@@ -1,5 +1,4 @@
 #include <Time.h>
-
 #include <SD.h>
 #include "Ultrasonic.h"
 #include <FileIO.h>
@@ -8,12 +7,14 @@ Ultrasonic ultrasonic (12,13);
 const int ledPin = 3;
 const int buttonPin = 2; 
 const int chipSelect = 53;
-File dataFile;
 
+File dataFile;
 int ledState = LOW;
 int buttonState = 0;
+int objectPassedCount = 0;
 boolean writeToggle = false;
 boolean buttonRelease = true;
+boolean canCount = true;
 
 void setup() {
    Serial.begin(9600);
@@ -34,50 +35,110 @@ void initSD() {
   Serial.println("card initialized.");
 }
 
-void loop()
-{
+
+//Performs any actions that will happen if the button is pushed
+void buttonCheck(){
   
-buttonState = digitalRead(buttonPin);
-
-if(buttonState == HIGH && buttonRelease )
-{
-  buttonRelease = false;
-  writeToggle = !writeToggle;
- 
-  if(ledState == HIGH)
-  {
-     ledState = LOW; 
-  }
-  else
-  {
-    ledState = HIGH;
-  }
-}
-
-if(buttonState == LOW)
-{
- buttonRelease = true; 
-}
-
-digitalWrite(ledPin, ledState);
-
-if(writeToggle)
-{
- dataFile = SD.open("datalog.txt", FILE_WRITE);
+  //Check if button is pushed down or not
+  buttonState = digitalRead(buttonPin);
   
- int distance = ultrasonic.Ranging(CM);
- 
- if(dataFile){ 
-
-   if (distance < 100) {
-    dataFile.print("d");
-    dataFile.print(distance);
-    Serial.print(distance);
-    Serial.print("t");
-    dataFile.close();
+  //If button is pushed and first time through loop while button is down, stops toggling if button is held down
+  if(buttonState == HIGH && buttonRelease)
+  {
+    //Set bool false, so this if statement doesn't loop
+    buttonRelease = false;
+    
+    //Toggle if SD is being written to
+    writeToggle = !writeToggle;
+    
+    //If writing was turned off, write to a file saying how many people it counting during the time it was writing
+    if(!writeToggle)
+    {
+      File objectPassCountFile = SD.open("objectPassCount.txt", FILE_WRITE);
+      objectPassCountFile.print("Objects counted: ");
+      objectPassCountFile.println(objectPassedCount);
+      objectPassCountFile.close();
+      
+      
+      //Reset counter for next round of counting
+      objectPassedCount = 0;
+    }
+   
+    //Turn LED on or off, to indicated if SD is being written to
+    if(ledState == HIGH)
+    {
+       ledState = LOW; 
+    }
+    else
+    {
+      ledState = HIGH;
     }
   }
-}
- delay(100);
   
-}
+  //If button isn't being pushed then enable the first if statement again
+  if(buttonState == LOW)
+  {
+   buttonRelease = true; 
+  }
+  
+} //End buttonCheck
+
+//Writes data into the SD card if writing is enabled and sensor picks up objects
+void writeToSD(){
+  
+  //If true, can write into SD card
+  if(writeToggle)
+  {
+    //Read in distance from sonar sensor
+    int distance = ultrasonic.Ranging(CM);
+    
+   //If distance is less than 2m
+   if (distance < 200) 
+   {
+     //Open the file or create and open file if it doesn't exist already.
+     dataFile = SD.open("datalog.txt", FILE_WRITE);
+      
+     //If there is a data file, write into it
+     if(dataFile)
+     { 
+       //Can only read 1 object until sensor reads nothing again, to avoid counting 1 object multiple times.
+       if(canCount)
+       {
+         objectPassedCount++;
+         canCount = false;
+         
+         //write to SD card
+         dataFile.print("d");
+         dataFile.print(distance);
+         dataFile.print(", ");
+        
+         //Write to serial
+         Serial.print(distance);
+       }
+    }
+    
+    //Close SD cards file
+    dataFile.close();
+   }//End of distance check
+   //Nothing is close enough to sensor, enable counting again.
+   else
+   {
+     canCount = true;
+   }
+ }
+}//End writeToSD
+
+void loop()
+{  
+  //Check for button press
+  buttonCheck();
+  
+  //Set LED to it's current state(lit or unlit)
+  digitalWrite(ledPin, ledState);
+  
+  //Try to write into SD card if enabled
+  writeToSD();
+  
+  delay(100);
+ 
+}//End loop
