8a9522b392f5d9d4fa67520f0e8cc9814928ad8f
Zane Unverferth <unverferthz@gmail.com>
2015-03-31 09:38:26 +1300

    sd card

--- sketch_feb25a/sketch_feb25a.ino
+++ sketch_feb25a/sketch_feb25a.ino
@@ -4,21 +4,28 @@
 #include "Ultrasonic.h"
 #include <FileIO.h>
 
-
-
 Ultrasonic ultrasonic (12,13);
+const int ledPin = 3;
+const int buttonPin = 2; 
 const int chipSelect = 53;
 File dataFile;
 
+int ledState = LOW;
+int buttonState = 0;
+boolean writeToggle = false;
+boolean buttonRelease = true;
+
 void setup() {
    Serial.begin(9600);
+   pinMode(ledPin, OUTPUT);
+   pinMode(buttonPin, INPUT);
    initSD();
 }
 
 void initSD() {
    Serial.print("Initializing SD card...");
    pinMode(10, OUTPUT);
-   
+ 
    if (!SD.begin(chipSelect)) {
     Serial.println("Card failed, or not present");
     // don't do anything more:
@@ -29,9 +36,35 @@ void initSD() {
 
 void loop()
 {
-
   
+buttonState = digitalRead(buttonPin);
+
+if(buttonState == HIGH && buttonRelease )
+{
+  buttonRelease = false;
+  writeToggle = !writeToggle;
+ 
+  if(ledState == HIGH)
+  {
+     ledState = LOW; 
+  }
+  else
+  {
+    ledState = HIGH;
+  }
+}
+
+if(buttonState == LOW)
+{
+ buttonRelease = true; 
+}
+
+digitalWrite(ledPin, ledState);
+
+if(writeToggle)
+{
  dataFile = SD.open("datalog.txt", FILE_WRITE);
+  
  int distance = ultrasonic.Ranging(CM);
  
  if(dataFile){ 
@@ -42,8 +75,9 @@ void loop()
     Serial.print(distance);
     Serial.print("t");
     dataFile.close();
-   }
- }
- 
-  delay(100);
+    }
+  }
+}
+ delay(100);
+  
 }
