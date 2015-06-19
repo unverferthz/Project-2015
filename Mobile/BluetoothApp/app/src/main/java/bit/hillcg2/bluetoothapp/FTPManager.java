7531640aff747e204b73af56d3e5753f002ad914
CGHill <cameron.hill2@hotmail.com>
2015-06-19 14:37:22 +1200

    Uploading file through SFTP working

---
+++ Mobile/BluetoothApp/app/src/main/java/bit/hillcg2/bluetoothapp/FTPManager.java
@@ -0,0 +1,149 @@
+package bit.hillcg2.bluetoothapp;
+
+import android.content.Context;
+import android.content.pm.PackageInstaller;
+import android.content.res.AssetManager;
+import android.os.AsyncTask;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpException;
+
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.w3c.dom.Text;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class FTPManager {
+    FTPClient mFTP;
+    JSch jsch;
+    AssetManager am;
+    DBManager dbManager;
+    Context context;
+
+    public FTPManager(Context startContext, DBManager startDBManager){
+        context = startContext;
+        am = context.getAssets();
+        dbManager = startDBManager;
+    }
+
+    public void sendFile(){
+        AsyncSendFile aSendFile = new AsyncSendFile();
+        aSendFile.execute();
+    }
+
+    private File createFileToSend(){
+        ArrayList<Incident> allIncidents = dbManager.getIncidents();
+
+        File outputFile = null;
+
+        try {
+            String fileName = "incidentData.txt";
+            FileOutputStream outputStream;
+            outputStream = context.openFileOutput(fileName, context.MODE_PRIVATE);
+            outputFile = new File(context.getFilesDir(), fileName);
+
+            int count = 0;
+
+            for (Incident i : allIncidents) {
+                String incidentString = "";
+
+                if(count != 0)
+                    incidentString += ",{";
+                else
+                    incidentString += "{";
+
+                String distance = String.valueOf(i.getDistance());
+                incidentString += "distance:" + distance + ",";
+
+                String time = i.getTime();
+                incidentString += "time:" + time + ",";
+
+                String date = i.getDate();
+                incidentString += "date:" + date + ",";
+
+                String lat = i.getLat();
+                incidentString += "latitude:" + lat + ",";
+
+                String lng = i.getLng();
+                incidentString += "longitude:" + lng + "}";
+
+                outputStream.write(incidentString.getBytes());
+                count++;
+            }
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return outputFile;
+    }
+
+    public class AsyncSendFile extends AsyncTask<Void,Void,Void>
+    {
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            String user = "unverzp1";
+            String password = "11000712";
+
+            mFTP = new FTPClient();
+            jsch = new JSch();
+
+            try
+            {
+                /*mFTP.connect("kate.ict.op.ac.nz", 46815);
+
+                mFTP.login(user, password);
+
+                mFTP.setFileType(FTP.ASCII_FILE_TYPE);
+                mFTP.enterLocalPassiveMode();*/
+
+                Session session = jsch.getSession(user, "kate.ict.op.ac.nz", 46815);
+                session.setConfig("PreferredAuthentications", "password");
+                session.setConfig("StrictHostKeyChecking", "no");
+                session.setPassword(password);
+                session.connect();
+                Channel channel = session.openChannel("sftp");
+                ChannelSftp sftp = (ChannelSftp)channel;
+                sftp.connect();
+
+
+                File outputFile = createFileToSend();
+
+                if(outputFile != null)
+                {
+                    // Open a FileInputStream to read your little file
+                    FileInputStream inputStream = new FileInputStream(outputFile);
+
+                    sftp.cd("/home/unverzp1/public_html");
+
+                    //mFTP.storeFile("incidentData.txt", inputStream);
+                    sftp.put(inputStream, "incidentData.txt", sftp.OVERWRITE);
+                }
+                sftp.disconnect();
+                session.disconnect();
+                //mFTP.disconnect();
+            }
+            catch(IOException e)
+            {
+                e.printStackTrace();
+            } catch (JSchException e) {
+                e.printStackTrace();
+            } catch (SftpException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+}
